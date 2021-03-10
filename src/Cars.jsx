fbd66ddb8e82e4375bfb2f0e058c770c0618ea64
Rosiele David <rosieleserpa@hotmail.com>
2021-03-10 19:56:27 -0300

    feat: add exercise 1

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,61 +1,45 @@
 // src/Cars.jsx
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
-    return (
-      <div>
-        <div>
-          <img
-            className={redCar ? 'car-right' : 'car-left'}
-            src={carRed}
-            alt="red car"
-          />
-          <button
-            onClick={() => moveCar('redCar', !redCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={blueCar ? 'car-right' : 'car-left'}
-            src={carBlue}
-            alt="blue car"
-          />
-          <button
-            onClick={() => moveCar('blueCar', !blueCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={yellowCar ? 'car-right' : 'car-left'}
-            src={carYellow}
-            alt="yellow car"
-          />
-          <button
-            onClick={() => moveCar('yellowCar', !yellowCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-      </div>
-    )
-  }
-};
+function Cars() {
+  const { cars, moveCard } = useContext(CarsContext);
+  const { red, blue, yellow } = cars;
 
-Cars.contextType = CarsContext;
+  return (
+    <main>
+      <section>
+        <img src={ carRed } alt="red car" className={ red ? 'car-right' : 'car-left' } />
+        <button type="button" onClick={ () => moveCard('red', !red) }>
+          Move
+        </button>
+      </section>
+      <section>
+        <img
+          src={ carBlue }
+          alt="blue car"
+          className={ blue ? 'car-right' : 'car-left' }
+        />
+        <button type="button" onClick={ () => moveCard('blue', !blue) }>
+          Move
+        </button>
+      </section>
+      <section>
+        <img
+          src={ carYellow }
+          alt="yellow car"
+          className={ yellow ? 'car-right' : 'car-left' }
+        />
+        <button type="button" onClick={ () => moveCard('yellow', !yellow) }>
+          Move
+        </button>
+      </section>
+    </main>
+  );
+}
 
 export default Cars;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,46 +1,34 @@
 // src/context/Provider.js
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
+function Provider({ children }) {
+  const [cars, setCars] = useState({
+    red: false,
+    blue: false,
+    yellow: false,
+  });
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
+  const moveCard = (car, side) => {
+    setCars({
+      ...cars,
+      [car]: side,
     });
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-    };
+  const contextValue = {
+    cars,
+    moveCard,
+  };
 
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
-};
+  return (
+    <CarsContext.Provider value={ contextValue }>
+      {children}
+    </CarsContext.Provider>
+  );
+}
 
 Provider.propTypes = {
   children: PropTypes.node.isRequired,
